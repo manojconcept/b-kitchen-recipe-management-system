13e84b2ce7c927445273834933ba07379a454ef8
manojconcept <manojveerapavu@gmail.com>
2024-04-11 17:38:26 +0530

    cors-orgin check

--- src/index.js
+++ src/index.js
@@ -10,17 +10,12 @@ const app = express();
 const port = 7000;
 const timeLog = moment().format('MMMM Do YYYY, h:mm:ss a');
 app.use(methodOverride("_method"));
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 
-app.options('*',cors());
-var allowCrossDomain = function(req,res,next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();  
-}
-app.use(allowCrossDomain);
+
 
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/recipes",recipesRoute);
